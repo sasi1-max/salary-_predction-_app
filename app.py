82d47c939d933ab81d6b53f9f6743cd8313f4445
sasi1-max <sasithota08@gmail.com>
2025-10-15 19:14:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import joblib
+import numpy as np
+st.title ("SALARY PREDCTION APP")
+
+st.divider()
+
+st.write("with this app you can get estimators for company salariies of the company employee")
+years = st.number_input("enter the years at company",value=1, step= 1, min_value= 0)
+jobrate = st.number_input("enter the jobrate",value=1, step=1, min_value=0)
+st.divider()
+x = [years, jobrate] 
+
+model = joblib.load("linear_model.pkl")
+
+predict = st.button("press the button for salary prediction")
+st.divider()
+
+
+if predict:
+    st.balloons()
+    x1 = np.array([x])
+
+    prediction = model.predict(x1)
+
+    st.write(f"salary predction is a{prediction}")
+
+
+else:
+    "please press button for prediction"
