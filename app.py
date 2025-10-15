b24ebc451f4895fc9ec8ce7c9fdf1c3263d1872e
sasi1-max <sasithota08@gmail.com>
2025-10-16 02:25:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.divider()
 
 st.write("with this app you can get estimators for company salariies of the company employee")
 years = st.number_input("Enter the years at company",value=1, step= 1, min_value= 0)
-jobrate = st.number_input("Enter the job",value=1, step=1, min_value=0,max_value=5)
+jobrate = st.number_input("Enter the job Rating",value=1, step=1, min_value=0,max_value=5)
 st.divider()
 x = [years, jobrate] 
 
@@ -29,4 +29,5 @@ if predict:
 else:
 
     "please press button for prediction"
+
 
